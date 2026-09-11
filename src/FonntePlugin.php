7fdc2b9a22849fab54cdb9f4f654aecc76854327
Muhammad Athhar Kautsar <athharkautsar14@gmail.com>
2026-09-11 13:43:22 +0700

    chore: bump Fonnte plugin to 0.2.8

--- src/FonntePlugin.php
+++ src/FonntePlugin.php
@@ -13,7 +13,7 @@ final class FonntePlugin extends Plugin
         return new PluginManifest(
             id: 'openkos/whatsapp-fonnte',
             name: 'Fonnte WhatsApp',
-            version: '0.2.7',
+            version: '0.2.8',
             description: 'Fonnte WhatsApp notification driver for OpenKOS.',
             coreVersion: '^0.2',
         );
